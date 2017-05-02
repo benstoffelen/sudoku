2efaffc4425ea5a89e18f1a1f4e3e9293668d73a
Ben Stoffelen <benstoffelen@gmail.com>
2017-05-02 22:49:13 +0200

    BeSt changes to make it run

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def load_data(is_train=True):
     import numpy as np
     quizzes = np.zeros((1000000, 81), np.int32)
     solutions = np.zeros((1000000, 81), np.int32)
-    for i, line in enumerate(open('sudoku.csv', 'r').read().splitlines()[1:]):
+    for i, line in enumerate(open('data/sudoku.csv', 'r').read().splitlines()[1:]):
         quiz, solution = line.split(",")
         for j, q_s in enumerate(zip(quiz, solution)):
             q, s = q_s
